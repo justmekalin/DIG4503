f5f1d8058eb8375efc17e75b44366a38738f1d67
Mekalin Paengwan <mekalinpaengwan@knights.ucf.edu>
2020-01-28 11:54:08 -0500

    lab2

--- lab2/src/components/HomePage/index.js
+++ lab2/src/components/HomePage/index.js
@@ -1,13 +1,9 @@
 import React from "react";
-import HomePageTitle from "../HomePageTitle";
 
 class HomePage extends React.Component {
     render() {
         return(
-            <div>
             <p>{this.props.firstName}</p>
-            <HomePageTitle />
-            </div>
         );      
     }
 }
