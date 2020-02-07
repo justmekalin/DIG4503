a9e4e7d12cf2d050a0d2103f6d5d8aa7dce2abf9
Mekalin Paengwan <mekalinpaengwan@knights.ucf.edu>
2020-02-06 21:56:42 -0500

    lab4

--- lab4/index.js
+++ lab4/index.js
@@ -1,10 +1,46 @@
+const Pokemon = require("json-pokemon");
+const chalk = require("chalk");
 const Express = require ("express");
 const App = Express();
-const port = 80
+const port = 80;
 
-App.get("/:id", (req, res) => {
-    res.send(req.params);
-});
+
+App.get("/id/:id", (req, res) => {
+    let result = { error: "That ID doesn't exist!" };
+  
+    Pokemon.forEach(value => {
+      if (value.id == req.params.id) {
+        result = value;
+        res.send(value);
+      }
+    });
+  
+    if (result.error) {
+      console.log(chalk.red(result.error));
+    } else {
+      console.log(chalk.green(req.path));
+    }
+  });
+  
+  App.get("/name/:name", (req, res) => {
+    let result = { error: "That name doesn't exist! (Case Sensitive)" };
+  
+    Pokemon.forEach(value => {
+      if (value.name == req.params.name) {
+        result = value;
+        res.send(value);
+      }
+    });
+  
+    if (result.error) {
+      // Invalid
+      console.log(chalk.red(result.error));
+    } else {
+      // Valid
+      console.log(chalk.green(req.path));
+    }
+  })
+
 
 App.listen(port, () => {
     console.log("Server Running!");
