26a84d2fe7c2e82c4840d79450e141fb23aa63df
Mekalin Paengwan <mekalinpaengwan@knights.ucf.edu>
2020-04-16 17:47:00 -0400

    added result component

--- lab7/client/src/App.js
+++ lab7/client/src/App.js
@@ -1,53 +1,18 @@
 import React from 'react';
-import YearSearch from './components/YearSearch';
+
 import Welcome from './components/Title';
+import ResultTable from './components/ResultTable';
 
 
 class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      "table": []
-    };
-
-  }
-
-  // Update the internal state.table
-  // (This is called from YearSearch.)
-  setResultsTable = (processed) => {
-    this.setState({"table": processed});
-  }
-
-  // Render the results table
-  createResultsTable() {
-
-    return (
-      <table>
-        <tbody>
-        {
-          this.state.table.map((entry, index) => {
-             return( 
-              <tr key={index}>
-                <td>{entry.title}</td>
-                <td>{entry.year}</td>
-              </tr>
-             );
-          })
-        }
-        </tbody>
-      </table>
-    );
-
-  }
 
   render() {
     return (
       <div>
    <Welcome />
-        <YearSearch callback={this.setResultsTable} />
-        {this.createResultsTable()}
+   <ResultTable />
+        
       </div>
     );
   }
